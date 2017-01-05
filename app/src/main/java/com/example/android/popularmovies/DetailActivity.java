6942b33a01d7863fa1358e915bc9704a6097d995
Anshdeep Singh <anshdeep.1494@gmail.com>
2017-01-05 22:08:30 +0530

    up navigation fix

--- app/src/main/java/com/example/android/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/android/popularmovies/DetailActivity.java
@@ -3,6 +3,7 @@ package com.example.android.popularmovies;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -98,5 +99,15 @@ public class DetailActivity extends AppCompatActivity {
 
         return outputDate;
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
